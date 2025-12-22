b02881925acc84f94a1bff4768f8cd4fa3a5bc5a
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-12-22 20:23:43 +0100

    fix: #main change /api/user PUT method enpoint

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -66,7 +66,7 @@ public class UserController : ControllerBase
         });
     }
 
-    [HttpPut("update-bio")]
+    [HttpPut]
     [Authorize]
     public async Task<ActionResult<UserDto>> UpdateProfile([FromBody] UpdateUserProfileDio dto)
     {
