bc52ba936746a5973a7abd9bcf507d31beb90100
Arkadiusz Cios <arkadiuszcios@outlook.com>
2025-12-23 18:35:16 +0100

    feat: #main add username to ranking endpoint

--- DTOs/User/TopUser.cs
+++ DTOs/User/TopUser.cs
@@ -3,6 +3,7 @@ namespace pyatform.DTOs.User;
 public class TopUser
 {
     public required string UserId { get; set; }
+    public required string UserName { get; set; }
     public string? ProfilePictureUrl { get; set; }
     public long ChallengesCount { get; set; }
 }
--- misc/top_users_function.sql
+++ misc/top_users_function.sql
@@ -1,12 +1,12 @@
 create or replace function top_users(
    top_limit int
 )
-RETURNS TABLE(UserId text, ProfilePictureUrl text, ChallengesCount bigint)
+RETURNS TABLE(UserId text, UserName varchar(256), ProfilePictureUrl text, ChallengesCount bigint)
 language plpgsql    
 as $$
 begin
     RETURN QUERY
-    SELECT u."Id", u."ProfilePictureUrl", Count(DISTINCT c."Id") FROM "AspNetUsers" u 
+    SELECT u."Id", u."UserName", u."ProfilePictureUrl", Count(DISTINCT c."Id") FROM "AspNetUsers" u 
     LEFT JOIN "Solutions" s ON s."UserId" = u."Id" AND s."HasPassedTests" = true
     LEFT JOIN "Challenges" c ON c."Id" = s."ChallengeId" 
     GROUP BY u."Id"
